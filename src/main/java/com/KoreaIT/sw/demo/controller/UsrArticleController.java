11da387ba5409b8090f0e9a551f704d11f93cd9d
aisiwonhae29 <aisiwonhae29@gmail.com>
2023-04-25 10:02:17 +0900

    what eat first commit

---
+++ src/main/java/com/KoreaIT/sw/demo/controller/UsrArticleController.java
@@ -0,0 +1,91 @@
+package com.KoreaIT.sw.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UsrArticleController {
+
+	
+//	@Autowired
+//	private ArticleService articleService;
+//
+//	@RequestMapping("/usr/article/doModify")
+//	@ResponseBody
+//	public ResultData<Integer> doModify(HttpServletRequest req, int id, String title, String body) {
+//		Rq rq = (Rq) req.getAttribute("rq");
+//
+//		Article article = articleService.getArticle(id);
+//		if (article == null) {
+//			return ResultData.from("F-1", Ut.f("%d번 글은 존재하지 않습니다", id), "id", id);
+//		}
+//
+//		ResultData actorCanModifyRd = articleService.actorCanModify(rq.getLoginedMemberId(), article);
+//
+//		if (actorCanModifyRd.isFail()) {
+//			return actorCanModifyRd;
+//		}
+//
+//		return articleService.modifyArticle(id, title, body);
+//
+//	}
+//
+//	@RequestMapping("/usr/article/doDelete")
+//	@ResponseBody
+//	public String doDelete(HttpServletRequest req, int id) {
+//		Rq rq = (Rq) req.getAttribute("rq");
+//
+//		Article article = articleService.getArticle(id);
+//		if (article == null) {
+//			return Ut.jsHitoryBack("F-1", Ut.f("%d번 글은 존재하지 않습니다", id));
+//		}
+//
+//		if (article.getMemberId() != rq.getLoginedMemberId()) {
+//			return Ut.jsHitoryBack("F-2", Ut.f("%d번 글에 대한 권한이 없습니다", id));
+//		}
+//
+//		articleService.deleteArticle(id);
+//
+//		return Ut.jsReplace(Ut.f("%d번 글을 삭제 했습니다", id), "../article/list");
+//	}
+//
+//	@RequestMapping("/usr/article/doWrite")
+//	@ResponseBody
+//	public ResultData<Article> doWrite(HttpServletRequest req, String title, String body) {
+//		Rq rq = (Rq) req.getAttribute("rq");
+//
+//		if (Ut.empty(title)) {
+//			return ResultData.from("F-1", "제목을 입력해주세요");
+//		}
+//		if (Ut.empty(body)) {
+//			return ResultData.from("F-2", "내용을 입력해주세요");
+//		}
+//
+//		ResultData<Integer> writeArticleRd = articleService.writeArticle(rq.getLoginedMemberId(), title, body);
+//
+//		int id = (int) writeArticleRd.getData1();
+//
+//		Article article = articleService.getArticle(id);
+//
+//		return ResultData.newData(writeArticleRd, "article", article);
+//	}
+//
+//	@RequestMapping("/usr/article/list")
+//	public String showList(Model model) {
+//		List<Article> articles = articleService.getForPrintArticles();
+//
+//		model.addAttribute("articles", articles);
+//
+//		return "usr/article/list";
+//	}
+//
+//	@RequestMapping("/usr/article/detail")
+//	public String showDetail(HttpServletRequest req, Model model, int id) {
+//		Rq rq = (Rq) req.getAttribute("rq");
+//		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
+//
+//		model.addAttribute("article", article);
+//
+//		return "usr/article/detail";
+//	}
+//
+}
---
+++ src/main/java/com/KoreaIT/sw/demo/controller/UsrMemberController.java
@@ -0,0 +1,113 @@
+package com.KoreaIT.sw.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UsrMemberController {
+	@RequestMapping("/usr/member/doJoin")
+	@ResponseBody
+	public String doJoin(String Id, String Pw) {
+		
+		return Id;
+	}
+//	@Autowired
+//	private MemberService memberService;
+//
+//	@RequestMapping("/usr/member/doJoin")
+//	@ResponseBody
+//	public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
+//			String email) {
+//
+//		if (Ut.empty(loginId)) {
+//			return ResultData.from("F-1", "아이디를 입력해주세요");
+//		}
+//		if (Ut.empty(loginPw)) {
+//			return ResultData.from("F-2", "비밀번호를 입력해주세요");
+//		}
+//		if (Ut.empty(name)) {
+//			return ResultData.from("F-3", "이름을 입력해주세요");
+//		}
+//		if (Ut.empty(nickname)) {
+//			return ResultData.from("F-4", "닉네임을 입력해주세요");
+//		}
+//		if (Ut.empty(cellphoneNum)) {
+//			return ResultData.from("F-5", "전화번호를 입력해주세요");
+//		}
+//		if (Ut.empty(email)) {
+//			return ResultData.from("F-6", "이메일을 입력해주세요");
+//		}
+//
+//		ResultData<Integer> joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNum, email);
+//
+//		if (joinRd.isFail()) {
+//			return (ResultData) joinRd;
+//		}
+//
+//		Member member = memberService.getMemberById(joinRd.getData1());
+//
+//		return ResultData.newData(joinRd, "member", member);
+//	}
+//
+//	@RequestMapping("/usr/member/login")
+//	public String showLogin(HttpSession httpSession) {
+//		return "usr/member/login";
+//	}
+//
+//	@RequestMapping("/usr/member/doLogin")
+//	@ResponseBody
+//	public String doLogin(HttpSession httpSession, String loginId, String loginPw) {
+//
+//		boolean isLogined = false;
+//
+//		if (httpSession.getAttribute("loginedMemberId") != null) {
+//			isLogined = true;
+//		}
+//
+//		if (isLogined) {
+//			return Ut.jsHitoryBack("F-5", "이미 로그인 상태입니다");
+//		}
+//
+//		if (Ut.empty(loginId)) {
+//			return Ut.jsHitoryBack("F-1", "아이디를 입력해주세요");
+//		}
+//		if (Ut.empty(loginPw)) {
+//			return Ut.jsHitoryBack("F-2", "비밀번호를 입력해주세요");
+//		}
+//
+//		Member member = memberService.getMemberByLoginId(loginId);
+//
+//		if (member == null) {
+//			return Ut.jsHitoryBack("F-3", Ut.f("%s는 존재하지 않는 아이디입니다", loginId));
+//		}
+//
+//		if (member.getLoginPw().equals(loginPw) == false) {
+//			return Ut.jsHitoryBack("F-4", Ut.f("비밀번호가 일치하지 않습니다"));
+//		}
+//
+//		httpSession.setAttribute("loginedMemberId", member.getId());
+//
+//		return Ut.jsReplace("S-1", Ut.f("%s님 환영합니다", member.getName()), "/");
+//	}
+//
+//	@RequestMapping("/usr/member/doLogout")
+//	@ResponseBody
+//	public ResultData doLogout(HttpSession httpSession) {
+//
+//		boolean isLogined = false;
+//
+//		if (httpSession.getAttribute("loginedMemberId") == null) {
+//			isLogined = true;
+//		}
+//
+//		if (isLogined) {
+//			return ResultData.from("F-1", "이미 로그아웃 상태입니다");
+//		}
+//
+//		httpSession.removeAttribute("loginedMemberId");
+//
+//		return ResultData.from("S-1", "로그아웃 되었습니다");
+//	}
+//
+}
